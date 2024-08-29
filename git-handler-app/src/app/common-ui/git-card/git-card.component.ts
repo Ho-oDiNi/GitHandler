0806594b6c716d973d84cf6136c012bdeb06ec7b
Ho-oDiNi <evgen5060@gmail.com>
2024-08-29 19:27:30 +0300

    feat: add listing card

--- git-handler-app/src/app/common-ui/git-card/git-card.component.ts
+++ git-handler-app/src/app/common-ui/git-card/git-card.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GitProfile } from '../../data/interfaces/git-profile.interface';
 
 @Component({
   selector: 'app-git-card',
   standalone: true,
   imports: [],
-  
+
   templateUrl: './git-card.component.html',
   styleUrl: './git-card.component.css'
 })
 export class GitCardComponent {
-
+  @Input() profile!: GitProfile;
 }
