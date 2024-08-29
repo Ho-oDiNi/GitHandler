35f26ef8fae13aca2a5fe7b0b7dacc5d0a2d0410
Ho-oDiNi <evgen5060@gmail.com>
2024-08-29 13:43:56 +0300

    feat: add git-card component

---
+++ git-handler-app/src/app/common-ui/git-card/git-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-git-card',
+  standalone: true,
+  imports: [],
+  
+  templateUrl: './git-card.component.html',
+  styleUrl: './git-card.component.css'
+})
+export class GitCardComponent {
+
+}
