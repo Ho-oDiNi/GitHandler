d98e59327ca23b9354172968e7e9af3f612fb8e5
Ho-oDiNi <evgen5060@gmail.com>
2024-08-30 09:05:30 +0300

    feat: add full functional

--- git-handler-app/src/app/app.component.ts
+++ git-handler-app/src/app/app.component.ts
@@ -1,26 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GitCardComponent } from './common-ui/git-card/git-card.component';
-import { InputFormComponent } from './common-ui/input-form/input-form.component';
 import { GitProfileService } from './data/services/git-profile.service';
 import { GitProfile } from './data/interfaces/git-profile.interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GitCardComponent, InputFormComponent],
+  imports: [RouterOutlet, GitCardComponent, FormsModule],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  user_name: string = "";
+
   gitProfileService = inject(GitProfileService)
   profiles: GitProfile[] = []
 
 
-  getGitCards = (user_name: string) => {
-    this.gitProfileService.getGitProfile(user_name)
+  getGitCards = () => {
+    console.log(this.user_name);
+    this.gitProfileService.getGitProfile(this.user_name)
     .subscribe(val => {
       this.profiles = val
     })
